e0692aa78d5b12d84f3ca679422f78f9ec13cc23
Quentin Ngo <quentinngo8@gmail.com>
2014-09-20 18:09:55 -0700

    User can change their vote

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    params[:per_page]	||= 1
+    params[:per_page]	||= 10
     params[:page]	||= 1
     @links = Link.order("created_at DESC").paginate(:page => params[:page], :per_page => params[:per_page])
   end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,9 @@ class VotesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @vote = current_user.votes.create(vote_params)
+    @vote = current_user.votes.where(:link_id => params[:vote][:link_id]).first
+    @vote ||= current_user.votes.create(vote_params)
+    @vote.update_attributes(:up => params[:vote][:up])
     redirect_to  :back
   end
 
