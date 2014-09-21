95fd0a9d4ab33070f1ebbab724b07b23f247423e
Quentin Ngo <quentinngo8@gmail.com>
2014-09-20 17:34:13 -0700

    Can vote on links

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,14 @@
+class VotesController < ApplicationController
+  before_filter :authenticate_user!
+
+  def create
+    @vote = current_user.votes.create(vote_params)
+    redirect_to  :back
+  end
+
+  private
+
+  def vote_params
+    params.require(:vote).permit(:user_id, :link_id, :up)
+  end
+end
